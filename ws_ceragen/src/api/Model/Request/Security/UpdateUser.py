02204ca0516505a74635c0f8e41dc5b378342eb0
Mickaell22 <mickaelmoranvera03@gmail.com>
2025-06-29 15:25:39 -0500

    Gestion de usuario completado
    Algunas funciones estaban incompletas , ya fue solucionado

--- ws_ceragen/src/api/Model/Request/Security/UpdateUser.py
+++ ws_ceragen/src/api/Model/Request/Security/UpdateUser.py
@@ -1,3 +1,9 @@
-from marshmallow import Schema,fields
+from marshmallow import Schema, fields
+
 class UpdateUserSchema(Schema):
     id_user = fields.Integer(required=True)
+    person_ci = fields.String(required=False)
+    person_mail = fields.String(required=False) 
+    person_password = fields.String(required=False)
+    rol_id = fields.Integer(required=False)
+    person_id = fields.Integer(required=False)
