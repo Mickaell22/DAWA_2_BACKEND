66d02cffb3ae9c022253302e117f5a813db14878
masm007 <marco.salazarmejia03@gmail.com>
2025-07-01 11:40:13 -0500

    Correcion de ClinicAllergyCatalog

--- ws_ceragen/src/api/Model/Request/Clinic/AllergyCatalogRequest.py
+++ ws_ceragen/src/api/Model/Request/Clinic/AllergyCatalogRequest.py
@@ -1,14 +1,12 @@
 from marshmallow import Schema, fields
 
 class AllergyCatalogInsertRequest(Schema):
-    alc_name = fields.String(required=True)
-    alc_description = fields.String(required=True)
-    alc_type_id = fields.Int(required=True)
+    al_name = fields.String(required=True)
+    al_description = fields.String(required=True)
     user_process = fields.String(required=True)
 
 class AllergyCatalogUpdateRequest(Schema):
-    alc_id = fields.Int(required=True)
-    alc_name = fields.String(required=True)
-    alc_description = fields.String(required=True)
-    alc_type_id = fields.Int(required=True)
+    al_id = fields.Int(required=True)
+    al_name = fields.String(required=True)
+    al_description = fields.String(required=True)
     user_process = fields.String(required=True)
